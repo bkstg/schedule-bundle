b03786d9a03072202ba7454e37b2a89f818c78f2
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-06-17 20:51:50 -0400

    Invitation menu item.

--- EventSubscriber/UserMenuSubscriber.php
+++ EventSubscriber/UserMenuSubscriber.php
@@ -3,17 +3,27 @@
 namespace Bkstg\ScheduleBundle\EventSubscriber;
 
 use Bkstg\CoreBundle\Event\UserMenuCollectionEvent;
-use Bkstg\CoreBundle\Event\MenuCollectionEvent;
 use Bkstg\ScheduleBundle\BkstgScheduleBundle;
+use Bkstg\ScheduleBundle\Entity\Invitation;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserMenuSubscriber implements EventSubscriberInterface
 {
     private $factory;
+    private $em;
+    private $token_storage;
 
-    public function __construct(FactoryInterface $factory) {
+    public function __construct(
+        FactoryInterface $factory,
+        EntityManagerInterface $em,
+        TokenStorageInterface $token_storage
+    ) {
         $this->factory = $factory;
+        $this->em = $em;
+        $this->token_storage = $token_storage;
     }
 
     public static function getSubscribedEvents()
@@ -22,11 +32,12 @@ class UserMenuSubscriber implements EventSubscriberInterface
         return [
            UserMenuCollectionEvent::NAME => [
                ['addScheduleMenuItem', 0],
+               ['addInvitationsMenuItem', -5],
            ],
         ];
     }
 
-    public function addScheduleMenuItem(MenuCollectionEvent $event)
+    public function addScheduleMenuItem(UserMenuCollectionEvent $event)
     {
         $menu = $event->getMenu();
 
@@ -38,5 +49,23 @@ class UserMenuSubscriber implements EventSubscriberInterface
             ],
         ]);
         $menu->addChild($schedule);
+    }
+
+    public function addInvitationsMenuItem(UserMenuCollectionEvent $event)
+    {
+        $menu = $event->getMenu();
+
+        $repo = $this->em->getRepository(Invitation::class);
+        $user = $this->token_storage->getToken()->getUser();
+        $invitations = $repo->findPendingInvitations($user);
+
+        $invitations = $this->factory->createItem('menu_item.invitations', [
+            'uri' => '#',
+            'extras' => [
+                'badge_count' => count($invitations),
+                'translation_domain' => BkstgScheduleBundle::TRANSLATION_DOMAIN,
+            ]
+        ]);
+        $menu->addChild($invitations);
     }
 }
--- Repository/InvitationRepository.php
+++ Repository/InvitationRepository.php
@@ -2,8 +2,34 @@
 
 namespace Bkstg\ScheduleBundle\Repository;
 
+use Bkstg\CoreBundle\User\UserInterface;
 use Doctrine\ORM\EntityRepository;
 
 class InvitationRepository extends EntityRepository
 {
+    public function findPendingInvitationsQuery(UserInterface $user)
+    {
+        $qb = $this->createQueryBuilder('i');
+        return $qb
+            ->join('i.event', 'e')
+
+            // Add conditions.
+            ->andWhere($qb->expr()->eq('e.active', ':active'))
+            ->andWhere($qb->expr()->gt('e.end', ':now'))
+            ->andWhere($qb->expr()->isNull('i.response'))
+            ->andWhere($qb->expr()->eq('i.invitee', ':invitee'))
+
+            // Add parameters.
+            ->setParameter('active', true)
+            ->setParameter('now', new \DateTime())
+            ->setParameter('invitee', $user->getUsername())
+
+            // Get query.
+            ->getQuery();
+    }
+
+    public function findPendingInvitations(UserInterface $user)
+    {
+        return $this->findPendingInvitationsQuery($user)->getResult();
+    }
 }
